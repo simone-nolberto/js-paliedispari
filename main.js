bd1696943286b07475fdfe9506783cf759185a05
simone-nolberto <simone.nolberto@outlook.com>
2024-02-27 16:14:39 +0100

    add
    esercizio pari e dispari main structure

--- main.js
+++ main.js
@@ -12,22 +12,66 @@
 //     console.log("Ritenta con un altra parola");
 // }
 
-const userWord = prompt("inserisci una parola")
+// const userWord = prompt("inserisci una parola")
 
-function reverseUserWord(userWord) {
+// function reverseUserWord(userWord) {
 
-    let reverseWord = userWord.split('').reverse().join('');
-    console.log(userWord);
+//     let reverseWord = userWord.split('').reverse().join('');
+//     console.log(userWord);
 
-    if (userWord === reverseWord) {
-        console.log("Questa parola è palindroma");
-    } else {
-        console.log("Ritenta con un altra parola");
-    }
+//     if (userWord === reverseWord) {
+//         console.log("Questa parola è palindroma");
+//     } else {
+//         console.log("Ritenta con un altra parola");
+//     }
 
-    return reverseWord
+//     return reverseWord
 
 
+// }
+
+// console.log(reverseUserWord(userWord));
+
+
+
+// esercizio Pari o dispari;
+
+const userChoice = prompt("Scegli pari o dispari?");
+console.log(userChoice);
+const userNumb = Number(prompt("Inserisci un numero da 1 a 5"));
+console.log(userNumb);
+
+function pcNumb(numb) {
+    let pcNumb = numb;
+    return pcNumb;
 }
 
-console.log(reverseUserWord(userWord));
+const pcRandomNumb = pcNumb(Math.floor(Math.random() * 5) + 1);
+console.log(pcRandomNumb);
+
+// let sum = userNumb + pcRandomNumb;
+// console.log(sum);
+
+function isSumEven(sum) {
+
+    let evenCheck = sum;
+
+    if (sum % 2 === 0) {
+        evenCheck = "pari";
+        // console.log("Questa somma è pari");
+    } else {
+        evenCheck = "dispari";
+        // console.log("Questa somma è dispari");
+    }
+
+    return evenCheck;
+}
+
+let checkResults = isSumEven(userNumb + pcRandomNumb);
+console.log(checkResults);
+
+if (userChoice === checkResults) {
+    console.log("Complimenti, hai vinto!");
+} else {
+    console.log("Spiacente, riprova!");
+}
--- readme.md
+++ readme.md
@@ -6,3 +6,22 @@ Creare una funzione per capire se la parola inserita è palindroma;
 - tramite prompt chiedo una parola all'utente;
 - con una funzione controllo se è palindroma;
 
+# Tool
+- let/const + prompt;
+- function;
+
+
+# Pari e Dispari Instructions
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+- Sommiamo i due numeri;
+- Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
+- Dichiariamo chi ha vinto.
+
+# Steps 
+- prompt "pari o dispari";
+- function "userNumb";
+- let "pcNumb" (Math.random);
+- let "sum" (userNumb + pcNumb);
+- function (somma pari o dispari?);
+- log the winner;
